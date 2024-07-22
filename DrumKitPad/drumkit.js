8b02442ddc91b32f5d2d4b0306544ae486ec6248
Teenyee <3577728579@qq.com>
2024-07-22 18:57:39 +0800

    Add files via upload

---
+++ DrumKitPad/drumkit.js
@@ -0,0 +1,30 @@
+let audioE1 = new Audio("drumkitBag/LoKick.wav");
+let audioE2 = new Audio("drumkitBag/Pierre Clap.wav");
+let audioE3 = new Audio("drumkitBag/TM-88 Hats [Open Hat 4].wav");
+let audioE4 = new Audio("drumkitBag/Izak Hat.wav");
+let audioE5 = new Audio("drumkitBag/BRENTRAMBO SNARE.wav");
+let audioE6 = new Audio("piano.MP3");
+function playSound1() {
+  audioE1.play();
+  audioE1.currentTime = 0;
+}
+function playSound2() {
+  audioE2.play();
+  audioE2.currentTime = 0;
+}
+function playSound3() {
+  audioE3.play();
+  audioE3.currentTime = 0;
+}
+function playSound4() {
+  audioE4.play();
+  audioE4.currentTime = 0;
+}
+function playSound5() {
+  audioE5.play();
+  audioE5.currentTime = 0;
+}
+function playSound6() {
+  audioE6.play();
+  audioE6.currentTime = 0;
+}
